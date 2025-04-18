da6467e44ce32fd7e3cca4a50257f06ebaf8ebed
Hassan Mehmood <ihassan463m@gmail.com>
2025-04-18 11:34:08 +0500

    add

--- src/app/page.js
+++ src/app/page.js
@@ -3,17 +3,17 @@ import React, { useState, useEffect } from 'react';
 // src/app/page.js
 // import React from 'react';
 import LoadingScreen from './components/LoadingScreen';
-import Menus from './components/Menus';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
+import Menus from './components/Menus.js';
+import About from './components/About.js';
+import Projects from './components/Projects.js';
+import Contact from './components/Contact.js';
 // import Footer from './components/Footer';
-import Education from './components/Education';
+import Education from './components/Education.js';
 // import Skills from './components/Skills';
 import 'regenerator-runtime/runtime';
 import dynamic from "next/dynamic"
 import { Suspense } from "react"
-import Achievements from './components/Achievements';
+import Achievements from './components/Achievements.js';
 
 const UltimateSkillsSection = dynamic(() => import("../app/components/Skills"), {
   ssr: false,
