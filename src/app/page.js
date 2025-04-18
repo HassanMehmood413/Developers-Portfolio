9b754225fe6544cd6bdadae541fab8e0cbf6981c
Hassan Mehmood <ihassan463m@gmail.com>
2025-04-18 11:31:49 +0500

    fixes

--- src/app/page.js
+++ src/app/page.js
@@ -7,9 +7,9 @@ import Menus from './components/Menus';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
 import Education from './components/Education';
-import Skills from './components/Skills';
+// import Skills from './components/Skills';
 import 'regenerator-runtime/runtime';
 import dynamic from "next/dynamic"
 import { Suspense } from "react"
